f79ef08360c995db6eeb3c73352f929fa1f47379
vitalika1234 <vitaliihoncharuk@gmail.com>
2020-10-17 09:45:49 +0300

    Updates

---
+++ precache-manifest.f2cec806fc106309d23990f0998dab2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a876e8675f0171380dd79ab957002cd5",
+    "url": "/React_RoboApp/index.html"
+  },
+  {
+    "revision": "c9e84264741b36bff3ad",
+    "url": "/React_RoboApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0fa7895678c597bac65b",
+    "url": "/React_RoboApp/static/css/main.216c8e55.chunk.css"
+  },
+  {
+    "revision": "c9e84264741b36bff3ad",
+    "url": "/React_RoboApp/static/js/2.d8fb72df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_RoboApp/static/js/2.d8fb72df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa7895678c597bac65b",
+    "url": "/React_RoboApp/static/js/main.436e3852.chunk.js"
+  },
+  {
+    "revision": "433c78e5dc33604e9d84",
+    "url": "/React_RoboApp/static/js/runtime-main.566c5c2b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React_RoboApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
