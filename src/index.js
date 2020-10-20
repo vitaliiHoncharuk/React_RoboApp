0372412e6c5047aa18b3d3231de35fc97e06cae5
vitalika1234 <vitaliihoncharuk@gmail.com>
2020-10-20 08:26:12 +0300

    remove store from App component

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const store = createStore(rootReducer, applyMiddleware(logger, thunkMiddleware))
 
 ReactDOM.render(
     <Provider store={store}>
-      <App store={store}/>
+      <App/>
     </Provider>,
   document.getElementById('root')
 );
