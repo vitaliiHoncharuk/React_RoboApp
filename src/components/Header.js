aac0f952ccf1e46d1e3785484ae43e90ebcced48
vitalika1234 <vitaliihoncharuk@gmail.com>
2020-10-22 08:43:53 +0300

    example of optimizations

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React, {PureComponent} from 'react';
+
+class Header extends PureComponent {
+
+  render() {
+    return <h1 className="f2">RoboFriends</h1>
+  }
+}
+
+export default Header;
